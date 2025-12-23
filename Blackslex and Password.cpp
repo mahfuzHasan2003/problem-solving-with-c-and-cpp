c8dbb38c7479885ffdf42c9c97be14166097f3d7
mahfuzhasan2003 <mahfuzhasan2003@yahoo.com>
2025-12-23 21:10:44 +0600

    solved a problem from Codeforces Round 1071 (Div. 3)

---
+++ Blackslex and Password.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int test_cases;
+   cin >> test_cases;
+   while (test_cases--)
+   {
+      int k, x;
+      cin >> k >> x;
+      cout << k * x + 1 << endl;
+   }
+}
