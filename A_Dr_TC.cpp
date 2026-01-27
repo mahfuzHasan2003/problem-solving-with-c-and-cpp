54464f2853f77f57cd83000e6b3bd426120ff348
mahfuzhasan2003 <mahfuzhasan2003@yahoo.com>
2026-01-27 10:15:12 +0600

    solved a math implementation problem

---
+++ A_Dr_TC.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int t;
+   cin >> t;
+   while (t--)
+   {
+      int n;
+      cin >> n;
+      string s;
+      cin >> s;
+      int cnt = 0;
+      for (char ch : s)
+      {
+         if (ch == '1')
+            cnt++;
+      }
+
+      int ans = 0;
+      for (char ch : s)
+      {
+         if (ch == '1')
+            ans += (cnt - 1);
+         else
+            ans += (cnt + 1);
+      }
+
+      cout << ans << endl;
+   }
+
+   return 0;
+}
